32f73b4ec591cfba9638044a96354d63e794be6e
ghostxbh <ghostxbh@hotmail.com>
2019-08-20 21:22:19 +0800

    node shell

---
+++ node-pro-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source /etc/profile
+echo "nodejs项目脚本"
+
+read -p "输入项目名：" project_name
+echo "项目名：$project_name"
+
+read -p "输入项目存放路径：" pro_path
+echo "项目存放路径：$pro_path"
+
+cd $pro_path
+
+echo "开始git克隆项目"
+read -p "输入git克隆地址：" git_path
+echo "git地址：$git_path"
+git clone $git_path
+echo "已克隆完成"
+
+echo "开始构建web项目"
+npm install
+
+pm2 start ./bin/www.js
+
+echo "项目已启动"
+
