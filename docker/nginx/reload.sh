cefa29a272415bd355e1eaa5ead369f33b57e011
ghostxbh <ghostxbh@hotmail.com>
2019-09-04 22:57:00 +0800

    add docker ngixn reload.sh

---
+++ docker/nginx/reload.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#docker kill ningx container and restart container
+
+container_id=`docker ps --filter 'name=nginx' | awk 'NR>1' | awk '{print $1}'`
+
+echo "kill nginx:"
+
+if [ -n "$container_id" ]; then
+   docker kill $container_id
+fi
+
+docker restart $container_id
+
+echo "nginx 重启完成"
