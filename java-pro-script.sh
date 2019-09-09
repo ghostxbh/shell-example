2109fab2dea0b4d6dfef1a191ba09a2e1e62aac9
ghostxbh <ghostxbh@hotmail.com>
2019-09-09 22:09:38 +0800

    add java script

---
+++ java-pro-script.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#java start script
+
+read -p "project name: " projectName
+
+read -p "project path: " projectPath
+
+jar="${projectName}.jar"
+
+cd ${projectPath}
+
+git pull
+mvn clean install
+
+port=`ps -ef | grep "$jar" | grep -v 'grep' | awk '{print $2}'`
+echo "kill $port"
+if [ -n "${port}" ]; then
+kill -9 ${port}
+fi
+
+cd ${projectPath}/target
+
+(nohup java -jar $jar nohup.out 2>&1) &
+sleep 5s
+ps -ef | grep $jar | grep -v 'grep' | awk '{print $2}'
+
+echo "项目已启动"
