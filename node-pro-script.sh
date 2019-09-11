22864f11801d1eb6da0276db640efecc8999c7a0
ghostxbh <ghostxbh@hotmail.com>
2019-09-11 13:50:01 +0800

    update node script

--- node-pro-script.sh
+++ node-pro-script.sh
@@ -8,12 +8,12 @@ echo "项目名：$project_name"
 read -p "输入项目存放路径：" pro_path
 echo "项目存放路径：$pro_path"
 
-cd $pro_path
+cd ${pro_path}
 
 echo "开始git克隆项目"
 read -p "输入git克隆地址：" git_path
 echo "git地址：$git_path"
-git clone $git_path
+git clone ${git_path}
 echo "已克隆完成"
 
 echo "开始构建web项目"
@@ -23,7 +23,7 @@ npm install
 pm2 start ./bin/www.js
 read -p "输入启动目录，默认（./bin/www.js）：" start_path
 if [$start_path]; then
-    pm2 start $start_path
+    pm2 start ${start_path}
 else
     pm2 start ./bin/www.js
 fi
