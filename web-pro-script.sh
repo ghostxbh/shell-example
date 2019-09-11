99a4b70e1ea45ff6826c292fe0585d88202c0b17
ghostxbh <ghostxbh@hotmail.com>
2019-09-11 13:50:16 +0800

    update web script

--- web-pro-script.sh
+++ web-pro-script.sh
@@ -6,15 +6,15 @@ read -p "输入项目名：" project_name
 echo "项目名：$project_name"
 
 read -p "输入项目存放路径：" pro_path
-dirpath=$pro_path/$project_name
+dirpath=${pro_path}/${project_name}
 echo "项目存放路径：$dirpath"
 
-cd $dirpath
+cd ${dirpath}
 
 echo "开始git克隆项目"
 read -p "输入git克隆地址：" git_path
 echo "git地址：$git_path"
-git clone $git_path
+git clone ${git_path}
 echo "已克隆完成"
 
 echo "开始构建web项目"
@@ -22,18 +22,18 @@ npm install
 npm run build
 
 read -p "输入web项目地址：" dest
-dest_path=$dest/$project_name
+dest_path=${dest}/${project_name}
 
-rm -rf $dest_path
-mkdir -p $dest_path
-cp -R dist/* $dest_path
+rm -rf ${dest_path}
+mkdir -p ${dest_path}
+cp -R dist/* ${dest_path}
 echo "web项目已构建"
 echo "================="
 
 echo "增加nginx配置"
 read -p "输入nginx配置地址：" nginx_path
 echo "nginx地址：$nginx_path"
-cd $nginx_path
+cd ${nginx_path}
 
 read -p "输入访问端口号：" port
 echo "访问端口号：$port"
@@ -49,7 +49,7 @@ echo "server {
       try_files /\$uri /\$uri/ /index.html;
     }
    error_page   502  /502.html;
-}"  >> $pro_nginx_name
+}"  >> ${pro_nginx_name}
 
 nginx -t
 nginx -s reload
